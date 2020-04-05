31783ff28a11852c62fba85a9efa6badb9ed4977
elminster <kamase79@daum.net>
2020-04-04 22:56:28 -0700

    lsp tunning

--- layers/+performance/packages.el
+++ layers/+performance/packages.el
@@ -1,4 +1,9 @@
 ;; packages.el --- performance for elminster.
 
+(defun performance/lsp-tuning ()
+  "lsp client/server communication generates a log of memory/gabage."
+  (setq gc-cons-threshold 100000000)
+  (setq read-process-output-max (* 1024 2048)))
+
 (defun performance/init ()
-)
+  (performance/lsp-tuning))
