b3f893966b7a824c9b1fa761e4eb9f5d32ce6915
elminster <kamase79@daum.net>
2020-04-05 09:57:21 -0700

    cosmetic update

--- layers/+tools/packages.el
+++ layers/+tools/packages.el
@@ -70,13 +70,51 @@
   "lsp mode"
   (use-package lsp-mode
   :commands lsp
-  :ensure t))
+  :ensure t
+  :custom ((lsp-auto-guess-root t)
+	   (lsp-enable-snippet nil)
+	   (lsp-prefer-flymake nil))
+  :hook ((python-mode c-mode c++-mode) . lsp)
+  :config
+  ;; Prefer using lsp-ui (flycheck) over flymake.
+  (setq lsp-prefer-flymake nil)))
 
 (defun tools/lsp-ui ()
   "lsp mode"
   (use-package lsp-ui
+    :requires lsp-mode flycheck
     :commands lsp-ui-mode
-    :ensure t))
+    :ensure t
+    :custom-face
+    (lsp-ui-doc-background ((t (:background nil))))
+    (lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic)))))
+    :bind (:map lsp-ui-mode-map
+		([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+		([remap xref-find-references] . lsp-ui-peek-find-references)
+		("C-c u" . lsp-ui-imenu))
+    :hook (lsp-mode-hook . lsp-ui-mode)
+    :custom((lsp-ui-doc-enable t)
+	    (lsp-ui-doc-use-childframe t)
+	    (lsp-ui-flycheck-enable t)
+	    (lsp-ui-flycheck-list-position 'right)
+	    (lsp-ui-flycheck-live-reporting t)
+	    (lsp-ui-peek-enable t)
+	    (lsp-ui-peek-list-width 60)
+	    (lsp-ui-peek-peek-height 25)
+	    (lsp-ui-doc-header t)
+	    (lsp-ui-doc-include-signature t)
+	    (lsp-ui-doc-position 'top)
+	    (lsp-ui-doc-border (face-foreground 'default))
+	    (lsp-ui-sideline-enable t)
+	    (lsp-ui-sideline-ignore-duplicate t)
+	    (lsp-ui-sideline-show-code-actions t))
+    :config
+    ;; Use lsp-ui-doc-webkit only in GUI
+    (setq lsp-ui-doc-use-webkit nil)
+    ;; WORKAROUND Hide mode-line of the lsp-ui-imenu buffer
+    ;; emacs-lsp/lsp-ui#243
+    (defadvice lsp-ui-imenu (after hide-lsp-ui-imenu-mode-line activate)
+      (setq mode-line-format nil))))
 
 (defun tools/company-lsp ()
   "company lsp"
