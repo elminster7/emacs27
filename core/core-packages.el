2f326f1c8a3edc793df7be265b8fd9b6d120c528
elminster <kamase79@daum.net>
2020-04-08 06:17:18 -0700

    added elpa repositories

--- core/core-packages.el
+++ core/core-packages.el
@@ -1,10 +1,11 @@
 ;; core-packages.el --- layer packages for elminster.
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa.milkbox" . "http://melpa.milkbox.net/packages/"))
-
 (package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa.milkbox" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/") t)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
