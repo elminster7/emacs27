090639f8054ef68bffc21c41feb7078fd6ff86ef
daum <kamase79@daum.net>
2020-04-14 01:19:44 +0900

    powerline evil theme

--- layers/+ui/packages.el
+++ layers/+ui/packages.el
@@ -2,12 +2,18 @@
   "vsdark+ theme init"
   (use-package vscdark-theme
     :ensure t
-    :init ((load-theme 'vscdark t))))
+    :init (custom-enabled-themes '(vsdark))))
+
+(defun ui/powerline-evil ()
+  "powerline evil init"
+  (use-package powerline-evil
+    :ensure t
+    :config (powerline-evil-center-color-theme)))
 
 ;; editor ui init
 (defun ui/function_init ()
   "ui preference settings init"
-  (ui/vsdark-theme))
+  (ui/powerline-evil))
 
 (defun ui/init ()
   (ui/function_init))
