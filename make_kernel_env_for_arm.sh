1bfabdec922266c6896bac3a31decbca33d087c5
elminster <perpect7@dags.kr>
2020-06-18 19:04:51 +0900

    qemu filesystem

--- make_kernel_env_for_arm.sh
+++ make_kernel_env_for_arm.sh
@@ -22,6 +22,24 @@ download_ubuntu_filesystem() {
     tar xf aarch64-laptops-bionic-prebuilt.img.xz
 }
 
+prepare_compress_image() {
+#    wget https://releases.linaro.org/aarch64-laptops/images/ubuntu/18.04/aarch64-laptops-bionic-prebuilt.img.xz
+#    unxz $PWD/aarch64-laptops-bionic-prebuilt.img.xz
+    FILESYSTEM=aarch64-laptops-bionic-prebuilt.img
+    EFI=`fdisk -l $PWD/$FILESYSTEM | grep EFI | awk '{print $2}'`
+    Linux=`fdisk -l $PWD/$FILESYSTEM | grep Linux | awk '{print $2}'`
+    EFI_SIZE=`expr $EFI \* 512`
+    Linux_Size=`expr $Linux \* 512`
+
+    if [ $1 == '1' ]; then
+	sudo mount -v -o offset=$EFI_SIZE -t vfat $FILESYSTEM mnt1
+    else if [ $1 == '2' ]; then
+	sudo mount -v -o offset=$Linux_Size -t ext4 $FILESYSTEM mnt2
+    fi
+    
+}
+
 #build_qemu
 #build_raspberry_kernel
-download_ubuntu_filesystem
+#download_ubuntu_filesystem
+prepare_compress_image
