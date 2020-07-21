cfd925bf63eb4cf6b0786920aa654d4b9df87bf5
elminster <perpect7@dags.kr>
2020-07-21 17:30:09 +0900

    spacemacs theme

--- init.el
+++ init.el
@@ -28,14 +28,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(vscdark))
+ '(custom-enabled-themes '(spacemacs-dark))
  '(custom-safe-themes
-   '("31f8d16d264e14e8e39c4f291e26cdd5516772a41660ef2ad895244c22024bd2" default))
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "67ea2e2549ab1ba9b27a4fc3c78353225d6fd155a69292dc6f7d3286bd8d3a82" "f4c8f0b999a6407211a899401315a628e1a5ae2f408c04a33b14d7aa3ed86187" "31f8d16d264e14e8e39c4f291e26cdd5516772a41660ef2ad895244c22024bd2" default))
  '(lsp-auto-guess-root t)
  '(lsp-enable-snippet nil)
  '(lsp-prefer-flymake nil t)
  '(package-selected-packages
-   '(vscdark-theme function-args equake vimish-fold evil-visual-mark-mode popwin counsel-projectile avy avy-menu yequake ecb minibuf-isearch neotree ripgrep elgrep popup-imenu e2wm multiple-cursors xcscope counsel-gtags ivy use-package))
+   '(spacemacs-theme sr-speedbar vscode-dark-plus-theme warm-night-theme helm-evil-markers helm-gtags eglot lsp-mode company-lsp magit vscdark-theme function-args equake vimish-fold evil-visual-mark-mode popwin counsel-projectile avy avy-menu yequake ecb minibuf-isearch neotree ripgrep elgrep popup-imenu e2wm multiple-cursors xcscope counsel-gtags ivy use-package))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
