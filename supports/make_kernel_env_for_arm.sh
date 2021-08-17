a19c247fc3631a41b68759f76d761788a523f944
elminster7 <kamase79@daum.net>
2021-08-18 01:37:42 +0900

    update env

--- supports/make_kernel_env_for_arm.sh
+++ supports/make_kernel_env_for_arm.sh
@@ -1,45 +1,68 @@
 #bin/sh
-sudo apt install -y gcc-aarch64-linux-gnu libglib2.0 libpixman-1-dev build-essential libssl-dev
+KERNEL_VER="5.10"
+QEMU_VER="6.0.0"
+ROOTFS="aarch64-laptops-bionic-prebuilt.img.xz"
 
+#===========================================================
+# requirement packages install.
+#===========================================================
+sudo apt install -y gcc-aarch64-linux-gnu libglib2.0-0 \
+     libpixman-1-dev build-essential libssl-dev ninja-build
+
+
+#===========================================================
+# install qemu
+#===========================================================
 build_qemu() {
-    wget https://download.qemu.org/qemu-5.0.0.tar.xz ~/
-    tar xf qemu-5.0.0.tar.xz
-    cd qemu-5.0.0
+    wget https://download.qemu.org/qemu-${QEMU_VER}.tar.xz ~/
+    tar xf qemu-${QEMU_VER}.tar.xz
+    cd qemu-${QEMU_VER}
     ./configure --target-list=aarch64-softmmu
     make -j $(nproc)
     make install
 }
 
-build_raspberry_kernel() {
-    git clone -b rpi-5.6.y https://github.com/raspberrypi/linux.git linux-5.6
-    cd linux-5.6
-    ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make bcm2711_defconfig
+build_raspberry4_kernel() {
+    #git clone -b rpi-${KERNEL_VER}.y https://github.com/raspberrypi/linux.git linux-${KERNEL_VER}
+    #cd linux-${KERNEL_VER}
+    #ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make bcm2711_defconfig
+    #ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make -j $(nproc)
+    git clone https://github.com/iamroot18/${KERNEL_VER}
+    cd linux-${KERNEL_VER}
+    ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make defconfig
     ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make -j $(nproc)
 }
 
-download_ubuntu_filesystem() {
-    wget https://releases.linaro.org/aarch64-laptops/images/ubuntu/18.04/aarch64-laptops-bionic-prebuilt.img.xz
-    tar xf aarch64-laptops-bionic-prebuilt.img.xz
-}
-
 prepare_compress_image() {
-#    wget https://releases.linaro.org/aarch64-laptops/images/ubuntu/18.04/aarch64-laptops-bionic-prebuilt.img.xz
-#    unxz $PWD/aarch64-laptops-bionic-prebuilt.img.xz
+    if [ ! -e aarch64-laptops-bionic-prebuilt.img.xz ]; then
+        wget https://releases.linaro.org/aarch64-laptops/images/ubuntu/18.04/${ROOTFS}
+    fi
+
+    #tar xf "${ROOTFS}"
+    unxz $PWD/${ROOTFS}
     FILESYSTEM=aarch64-laptops-bionic-prebuilt.img
     EFI=`fdisk -l $PWD/$FILESYSTEM | grep EFI | awk '{print $2}'`
     Linux=`fdisk -l $PWD/$FILESYSTEM | grep Linux | awk '{print $2}'`
     EFI_SIZE=`expr $EFI \* 512`
     Linux_Size=`expr $Linux \* 512`
 
-    if [ $1 == '1' ]; then
-	sudo mount -v -o offset=$EFI_SIZE -t vfat $FILESYSTEM mnt1
-    else if [ $1 == '2' ]; then
-	sudo mount -v -o offset=$Linux_Size -t ext4 $FILESYSTEM mnt2
-    fi
-    
+    mkdir mnt1 mnt2
+    sudo mount -v -o offset=$EFI_SIZE -t vfat $FILESYSTEM mnt1
+    sudo umount mnt1
+    sudo mount -v -o offset=$Linux_Size -t ext4 $FILESYSTEM mnt2
+    cd mnt2/
+    sudo tar cfv bio_rootfs.tar.gz *
 }
 
-#build_qemu
-#build_raspberry_kernel
-#download_ubuntu_filesystem
+make_rootfs_ext() {
+	dd if=/dev/zero of=rootfs.ext4 bs=1M count=16000
+	mkfs.ext4 -F rootfs.ext4
+	sudo mount rootfs.ext4 mnt1 -o loop
+	cd mnt2/
+	sudo tar xf bio_rootfs.tar.gz -C ../mnt1
+}
+
+build_qemu
+build_raspberry4_kernel
 prepare_compress_image
+make_rootfs_ext
